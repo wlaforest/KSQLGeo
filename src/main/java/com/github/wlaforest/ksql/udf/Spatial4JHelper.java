64f148cdc20cb55a91c04537a4b3eb3f715914d6
Will LaForest <lacapaneus@gmail.com>
2020-05-12 16:51:22 -0400

    Spherical model

--- src/main/java/com/github/wlaforest/ksql/udf/Spatial4JHelper.java
+++ src/main/java/com/github/wlaforest/ksql/udf/Spatial4JHelper.java
@@ -1,5 +1,6 @@
 package com.github.wlaforest.ksql.udf;
 
+import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.context.SpatialContextFactory;
 import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
 import org.locationtech.spatial4j.shape.Shape;
@@ -8,10 +9,12 @@ import org.locationtech.spatial4j.shape.SpatialRelation;
 public class Spatial4JHelper {
 
     // This will be lazily created when need a factory to support operations that are only available in 2D
-    private SpatialContextFactory scf2d;
+    private SpatialContextFactory scfEuclidean;
+    private SpatialContext scEuclidean;
 
     // This will be created for cylandrical or spherical operations
     private SpatialContextFactory scf;
+    private SpatialContext sc;
 
     public void Spatial4JEngine()
     {
@@ -37,30 +40,85 @@ public class Spatial4JHelper {
         geoShape1 = stringDeserializer.getSpatial4JShapeFromString(geo1);
         geoShape2 = stringDeserializer.getSpatial4JShapeFromString(geo2);
 
-
         SpatialRelation relation = geoShape1.relate(geoShape2);
         return relation.intersects();
     }
 
-    private Spatial4jStringDeserializer getDeserializer(boolean euclidean) {
-        Spatial4jStringDeserializer stringDeserializer;
+    /**
+     * Checks to see if the geo1 contains geo2 where the geos are shapes encoded in a string.
+     * This supports multiple string encodings like WKT and GeoJSON.  In theory any format supported by the underlying
+     * Spatial4j library.  Please check the Spatial4j documentation to see what shapes are supported in what models.
+     *
+     * @param geo1 geo shape one
+     * @param geo2 geo shape two
+     * @param euclidean Is this euclidean space?  If not it will try and support cylander or spherical
+     * @return Does geo1 contain geo2
+     */
+    public boolean contained(String geo1, String geo2, boolean euclidean) throws GeometryParseException {
+        Shape geoShape1;
+        Shape geoShape2;
 
-        // Lazy creation of factory if necessary
-        if (euclidean == true)
+        Spatial4jStringDeserializer stringDeserializer = getDeserializer(euclidean);
+
+        geoShape1 = stringDeserializer.getSpatial4JShapeFromString(geo1);
+        geoShape2 = stringDeserializer.getSpatial4JShapeFromString(geo2);
+
+        SpatialRelation relation = geoShape1.relate(geoShape2);
+        return relation == SpatialRelation.CONTAINS;
+    }
+
+    /**
+     * Checks to see if the geo1 contains the point specified by lat, lon. geo1 is a shape encoded in a string.
+     * This supports multiple string encodings like WKT and GeoJSON.  In theory any format supported by the underlying
+     * Spatial4j library.  Please check the Spatial4j documentation to see what shapes are supported in what models.
+     *
+     * @param geo1 geo shape one
+     * @param lat latitude for point to check contain inside of geo1
+     * @param lon longitude for point to check contain inside of geo1
+     * @param euclidean Is this euclidean space?  If not it will try and support cylander or spherical
+     * @return Does geo1 contain geo2
+     */
+    public boolean contained(String geo1, double lat, double lon, boolean euclidean) throws GeometryParseException {
+        Shape geoShape1;
+        Shape geoShape2;
+
+        Spatial4jStringDeserializer stringDeserializer = getDeserializer(euclidean);
+
+        geoShape1 = stringDeserializer.getSpatial4JShapeFromString(geo1);
+
+        if (euclidean)
         {
-            if (scf2d == null)
-                scf2d = new JtsSpatialContextFactory();
-
-            stringDeserializer = new Spatial4jStringDeserializer(scf2d);
+            geoShape2 = scfEuclidean.makeShapeFactory(scEuclidean).pointXY(lon,lat);
         }
         else
         {
-            if (scf == null)
-                scf = new SpatialContextFactory();
-
-            stringDeserializer = new Spatial4jStringDeserializer(scf);
+            geoShape2 = scfEuclidean.makeShapeFactory(sc).pointXY(lon, lat);
         }
-        return stringDeserializer;
+
+
+        SpatialRelation relation = geoShape1.relate(geoShape2);
+        return relation == SpatialRelation.CONTAINS;
     }
 
+
+    private Spatial4jStringDeserializer getDeserializer(boolean euclidean)
+    {
+        // Lazy creation of factory and context if necessary
+        if (euclidean == true)
+        {
+            if (scfEuclidean == null) {
+                this.scfEuclidean = new JtsSpatialContextFactory();
+                this.scEuclidean = scfEuclidean.newSpatialContext();
+            }
+            return new Spatial4jStringDeserializer(scfEuclidean,scEuclidean);
+        }
+        else
+        {
+            if (scf == null) {
+                this.scf = new SpatialContextFactory();
+                this.sc = scf.newSpatialContext();
+            }
+            return new Spatial4jStringDeserializer(scf, sc);
+        }
+    }
 }
--- src/main/java/com/github/wlaforest/ksql/udf/Spatial4jStringDeserializer.java
+++ src/main/java/com/github/wlaforest/ksql/udf/Spatial4jStringDeserializer.java
@@ -1,6 +1,5 @@
 package com.github.wlaforest.ksql.udf;
 
-import org.locationtech.jts.io.geojson.GeoJsonReader;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.context.SpatialContextFactory;
 import org.locationtech.spatial4j.io.GeoJSONReader;
@@ -10,23 +9,39 @@ import org.locationtech.spatial4j.shape.Shape;
 import java.io.StringReader;
 import java.text.ParseException;
 
-public class Spatial4jStringDeserializor {
+public class Spatial4jStringDeserializer {
 
-    protected static Shape getSpatial4JShapeFromString(String stringEncoding) throws GeometryParseException {
+    private SpatialContextFactory scf;
+    private SpatialContext sc;
 
-        SpatialContextFactory scf = new SpatialContextFactory();
-        SpatialContext sc = scf.newSpatialContext();
+    public Spatial4jStringDeserializer(SpatialContextFactory scf, SpatialContext sc)
+    {
+        this.scf = scf;
+        this.sc = sc;
+    }
+
+    public Shape getSpatial4JShapeFromString(String stringEncoding) throws GeometryParseException
+    {
+
+        Shape shape;
 
         WKTReader reader = new WKTReader(sc, scf);
+
         try {
-             return reader.parse(stringEncoding);
+             shape = reader.parse(stringEncoding);
+             if (shape != null) return shape;
         } catch (ParseException pe) {
-            GeoJSONReader gjr = new GeoJSONReader(sc,scf);
-            try {
-                gjr.read(new StringReader(stringEncoding));
-            } catch (Exception e) {
-                throw new GeometryParseException("Bad string encoding: " + stringEncoding);
-            }
+            // we will try geojson next
         }
+
+        GeoJSONReader gjr = new GeoJSONReader(sc,scf);
+        try {
+            shape = gjr.read(new StringReader(stringEncoding));
+            if (shape != null) return shape;
+        } catch (Exception e) {
+            throw new GeometryParseException("Bad string encoding: " + stringEncoding,e);
+        }
+
+        throw new GeometryParseException("Bad string encoding: " + stringEncoding);
     }
 }
