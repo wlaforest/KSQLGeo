e6d9274ea4c4dde59e6998f6f39dbc1ddf98feac
Will LaForest <lacapaneus@gmail.com>
2020-05-15 17:08:37 -0400

    Added GeoHash function

--- src/main/java/com/github/wlaforest/ksql/udf/GeoHash.java
+++ src/main/java/com/github/wlaforest/ksql/udf/GeoHash.java
@@ -1,4 +1,36 @@
 package com.github.wlaforest.ksql.udf;
 
-public class GeoHash {
+import io.confluent.ksql.function.udf.Udf;
+import io.confluent.ksql.function.udf.UdfDescription;
+import io.confluent.ksql.function.udf.UdfParameter;
+import org.locationtech.spatial4j.io.GeohashUtils;
+
+@UdfDescription(
+        name = "geo_hash",
+        description = "Function to calculate thee geohash of a given point.",
+        version = "1.1",
+        author = "Will LaForest"
+)
+public class GeoHash extends GeometryBase
+{
+    @Udf(description = "Function to calculate thee geohash of a given point.")
+    public String geo_hash(
+            @UdfParameter(value = "lat", description = "latitude")double lat,
+            @UdfParameter(value = "lat", description = "latitude")double lon) throws GeoHashBadParameterException {
+        if (lat < -90 || lat > 90 || lon < -180 || lon > 180)
+            throw new GeoHashBadParameterException("lat or lon are out of boundaries for -90 to 90 and -180 to 180");
+        return GeohashUtils.encodeLatLon(lat,lon);
+    }
+
+    @Udf(description = "Function to calculate thee geohash of a given point.")
+    public String geo_hash(double lat, double lon, int precision)
+    {
+        return GeohashUtils.encodeLatLon(lat,lon,precision);
+    }
+
+    public class GeoHashBadParameterException extends Exception {
+        public GeoHashBadParameterException(String s) {
+            super(s);
+        }
+    }
 }
--- src/test/java/com/github/wlaforest/ksql/udf/GeoHashTest.java
+++ src/test/java/com/github/wlaforest/ksql/udf/GeoHashTest.java
@@ -1,4 +1,35 @@
 package com.github.wlaforest.ksql.udf;
 
-public class GeoHashTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GeoHashTest
+{
+    @Test
+    void testSimple() throws GeoHash.GeoHashBadParameterException {
+        GeoHash gh = new GeoHash();
+        String result = gh.geo_hash(-82,76);
+        assertNotNull(result);
+    }
+
+    @Test
+    void testPrecion() throws GeoHash.GeoHashBadParameterException {
+        GeoHash gh = new GeoHash();
+        String result = gh.geo_hash(-82,76);
+        assertNotNull(result);
+    }
+
+    @Test
+    void testBadValues() {
+        GeoHash gh = new GeoHash();
+        try{
+            gh.geo_hash(-82032,74436);
+        }
+        catch ( GeoHash.GeoHashBadParameterException ghb)
+        {
+            return;
+        }
+
+        fail("bad paraameters should have thrown excpetion");
+    }
 }
