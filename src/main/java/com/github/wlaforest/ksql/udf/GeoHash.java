70c2d981a351188aad509d150b3d657f5a716ffa
Will LaForest <lacapaneus@gmail.com>
2020-07-08 15:38:43 -0400

    Enhanced the documentation

--- src/main/java/com/github/wlaforest/ksql/udf/GeoHash.java
+++ src/main/java/com/github/wlaforest/ksql/udf/GeoHash.java
@@ -7,23 +7,28 @@ import org.locationtech.spatial4j.io.GeohashUtils;
 
 @UdfDescription(
         name = "geo_hash",
-        description = "Function to calculate thee geohash of a given point.",
+        description = "Function to calculate the geohash of a given point.  Based on the Lucene code" +
+                "https://lucene.apache.org/core/5_5_0/spatial/org/apache/lucene/spatial/util/GeoHashUtils.html",
         version = "1.1",
         author = "Will LaForest"
 )
 public class GeoHash
 {
-    @Udf(description = "Function to calculate the geohash of a given point.")
+    @Udf(description = "Function to calculate the geohash of a given point.  Precision defaults to 12")
     public String geo_hash(
             @UdfParameter(value = "lat", description = "latitude")double lat,
-            @UdfParameter(value = "lat", description = "latitude")double lon) throws GeoHashBadParameterException {
+            @UdfParameter(value = "lat", description = "longitude")double lon) throws GeoHashBadParameterException {
         if (lat < -90 || lat > 90 || lon < -180 || lon > 180)
             throw new GeoHashBadParameterException("lat or lon are out of boundaries for -90 to 90 and -180 to 180");
         return GeohashUtils.encodeLatLon(lat,lon);
     }
 
     @Udf(description = "Function to calculate thee geohash of a given point.")
-    public String geo_hash(double lat, double lon, int precision)
+    public String geo_hash(
+            @UdfParameter(value = "lat", description = "latitude")double lat,
+            @UdfParameter(value = "lon", description = "longitude")double lon,
+            @UdfParameter(value = "precision", description = "what level of precision?  Goes from 1-12")
+                    int precision)
     {
         return GeohashUtils.encodeLatLon(lat,lon,precision);
     }
