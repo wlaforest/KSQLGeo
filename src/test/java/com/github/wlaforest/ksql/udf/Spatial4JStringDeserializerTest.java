ad9fc9e7d1d5cbed17d769026e9ac223a48cb289
wlaforest <lacapaneus@gmail.com>
2021-07-29 17:08:43 -0400

    Removing Spherical tests until we add this in the future

--- src/test/java/com/github/wlaforest/ksql/udf/Spatial4JStringDeserializerTest.java
+++ src/test/java/com/github/wlaforest/ksql/udf/Spatial4JStringDeserializerTest.java
@@ -2,7 +2,6 @@ package com.github.wlaforest.ksql.udf;
 
 import org.junit.jupiter.api.Test;
 import org.locationtech.spatial4j.context.SpatialContextFactory;
-import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
 import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
 import org.locationtech.spatial4j.shape.Shape;
 
@@ -14,33 +13,8 @@ class Spatial4JStringDeserializerTest {
     public void testWktEuclidean() throws GeometryParseException {
         SpatialContextFactory scf = new JtsSpatialContextFactory();
         Spatial4jStringDeserializer ssd = new Spatial4jStringDeserializer(scf, scf.newSpatialContext());
-        Shape flintHill = ssd.getSpatial4JShapeFromString(TestStrings.FLINT_HILL);
+        Shape flintHill = ssd.getSpatial4JShapeFromString(TestStrings.FLINT_HILL_WKT);
         assertNotNull(flintHill);
         assertFalse(flintHill.isEmpty());
-    }
-
-    @Test
-    public void testWktSphere() throws GeometryParseException {
-        SpatialContextFactory scf = new SpatialContextFactory();
-        Spatial4jStringDeserializer ssd = new Spatial4jStringDeserializer(scf, scf.newSpatialContext());
-        Shape flintHill = ssd.getSpatial4JShapeFromString(TestStrings.FLINT_HILL_POINT);
-        assertNotNull(flintHill);
-        assertFalse(flintHill.isEmpty());
-    }
-
-    @Test
-    public void testUnsupportedShapeWktSphere() throws GeometryParseException {
-        SpatialContextFactory scf = new SpatialContextFactory();
-        Spatial4jStringDeserializer ssd = new Spatial4jStringDeserializer(scf, scf.newSpatialContext());
-        try {
-            Shape flintHill = ssd.getSpatial4JShapeFromString(TestStrings.FLINT_HILL);
-            assertNotNull(flintHill);
-        }
-        catch (GeometryParseException gpe)
-        {
-            // Success
-            return;
-        }
-        fail("Shape " + TestStrings.FLINT_HILL + " Should not have been supported in Spherical model");
     }
 }
