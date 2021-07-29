6e1c7b4f068f2b1efb35d9001e77b04642664ce6
wlaforest <lacapaneus@gmail.com>
2021-07-29 16:58:25 -0400

    Removing demo files

--- demo/loadData.sh
+++
@@ -1,3 +0,0 @@
-java -jar jars/avro-tools-1.9.2.jar tojson data/raw20150421_sample.avro | \
-  kafka-avro-console-producer --broker-list localhost:9092 --topic opensky \
-  --property value.schema.file=data/opensky.avsc
